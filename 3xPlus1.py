83884e64c057deb29eae3e83ca6d23cbd96d22cf
ApophisLotuw <Fleurkejj@gmail.com>
2021-09-04 01:20:46 +0200

    1

--- 3xPlus1.py
+++ 3xPlus1.py
@@ -1,1 +1,30 @@
-print("Hoi GJ")
+from sys import exit
+
+
+def collatz(number, round):
+    if number == 1:
+        return 1
+    elif (number % 2) > 0:
+        result = 3 * number + 1
+    else:
+        result = int(number / 2)
+    print("Ronde: {}, oude getal: {}, nieuwe getal: {}".format(round, number, result))
+    return (result, round+1)
+
+
+def main():
+    getal = None
+    start = 1
+
+    try:
+        getal = int(input("Vul begin-getal in: "))
+    except ValueError:
+        print("Oeps, graag een positief heel getal boven 0 invullen.")
+        exit(1)
+
+    while getal != 1:
+        (getal, start) = collatz(getal, start)
+
+
+if __name__ == "__main__":
+    main()
