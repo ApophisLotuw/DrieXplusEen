1f68e0d5b0bfbd225c005f4df730462cf9a57e38
ApophisLotuw <Fleurkejj@gmail.com>
2021-09-10 09:14:08 +0200

    1

--- 3xPlus1.py
+++ 3xPlus1.py
@@ -1,30 +1,54 @@
-from sys import exit
+# from sys import exit
 
+import sys
 
-def collatz(number, round):
-    if number == 1:
-        return 1
-    elif (number % 2) > 0:
-        result = 3 * number + 1
+def berekening():
+
+begingetal = 500
+
+try:
+    if (begingetal % 2) > 0:
+        begingetal = 3 * begingetal + 1
+        print("Begingetal = ", begingetal)
+
+    elif begingetal == 1:
+        begingetal = begingetal + 1
+        print("Begingetal = ", begingetal)
+
     else:
-        result = int(number / 2)
-    print("Ronde: {}, oude getal: {}, nieuwe getal: {}".format(round, number, result))
-    return (result, round+1)
+        begingetal = int(begingetal / 2)
+        print("Begingetal = ", begingetal)
 
+return (begingetal)
 
-def main():
-    getal = None
-    start = 1
+except:
+    sys.exit(1)
 
-    try:
-        getal = int(input("Vul begin-getal in: "))
-    except ValueError:
-        print("Oeps, graag een positief heel getal boven 0 invullen.")
-        exit(1)
+# def collatz(number, round):
+#    if number == 1:
+#        return 1
+#    elif (number % 2) > 0:
+#        result = 3 * number + 1
+#    else:
+#        result = int(number / 2)
+#    print("Ronde: {}, oude getal: {}, nieuwe getal: {}".format(round, number, result))
+#    return (result, round+1)
 
-    while getal != 1:
-        (getal, start) = collatz(getal, start)
+#def main():
+#    getal = 1
+#    start = 1
 
+#    try:
+#        collatz(getal, start)
 
-if __name__ == "__main__":
-    main()
+#    try:
+#        getal = int(input("Vul begin-getal in: "))
+#    except ValueError:
+#        print("Oeps, graag een positief heel getal boven 0 invullen.")
+#        exit(1)
+
+#    while getal != 1:
+#        (getal, start) = collatz(getal, start)
+
+#    if __name__ == "__main__":
+#        main()
