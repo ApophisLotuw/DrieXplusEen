b1437080bfea532ea9786b02b0d72c49e3195456
apophislotuw <Fleurkejj@gmail.com>
2021-09-14 19:08:59 +0200

    1

--- 3xPlus1.py
+++ 3xPlus1.py
@@ -2,11 +2,8 @@ def main():
     getal = int(input())
     ronde = 1
 
-    while (getal > 1):
-        if (getal == 1):
-            print("Rondes gebruikt: ", ronde)
-            exit()
-        elif (int(getal % 2)) > 0:
+    while getal > 1:
+        if ((getal % 2)) > 0:
             x = int(oneven(getal))
             print("Oneven:", x, " Ronde: ", ronde)
             ronde = ronde + 1
@@ -17,16 +14,19 @@ def main():
             ronde = ronde + 1
             getal = x
     else:
-        print("Klaar")
+        ronde = ronde - 1
+        print("Rondes gebruikt: ", ronde)
 
 
 def even(getal2):
     getal2 = int(getal2 / 2)
     return getal2
 
+
 def oneven(getal2):
     getal2 = 3 * getal2 + 1
     return getal2
+
 
 if __name__ == "__main__":
     main()
